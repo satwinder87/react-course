30cf6263e97231316dc2bd12508314655a49ee08
Satwinder <satwinder.singh@telenor.se>
2022-02-26 15:26:16 +0100

    Passing state from child to parent component.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
-  //const [enteredTitle, setEnteredTitle] = useState('');
-  //const [enteredAmount, setEnteredAmount] = useState('');
-  //const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
+  /*
   let stateObject = {
     enteredTitle: '',
     enteredAmount: '',
@@ -14,51 +15,78 @@ const ExpenseForm = () => {
   };
 
   const [state, setState] = useState(stateObject);
-
+*/
   const titleChangeHandler = (event) => {
-    //setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     //console.log(enteredTitle);
     /*
   setState({
     ...state,
     enteredTitle: event.target.value,
-  });*/
+  });
     setState((prevState) => {
       return {...prevState, enteredTitle: event.target.value};
     });
     console.log(state);
+     */
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    // console.log(enteredAmount);
-    setState((prevState) => {
-      return {...prevState, enteredAmount: event.target.value};
-    });
-    console.log(state);
+    setEnteredAmount(event.target.value);
+    //console.log(enteredAmount);
+    /*
+   setState((prevState) => {
+     return {...prevState, enteredAmount: event.target.value};
+   });
+   console.log(state);
+    */
   };
 
   const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // console.log(enteredDate);
+
+    /*
+
     setState((prevState) => {
       return {...prevState, enteredDate: event.target.value};
     });
     console.log(state);
+
+     */
   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+
+  };
+
   return (
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type='text' onChange={titleChangeHandler}/>
+            <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type='number' min="0.01" step="0.01"
+            <input type='number' value={enteredAmount} min="0.01" step="0.01"
                    onChange={amountChangeHandler}/>
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type='date' min="2019-01-01" max="2022-12-31"
+            <input type='date' value={enteredDate} min="2019-01-01" max="2022-12-31"
                    onChange={dateChangeHandler}/>
           </div>
         </div>
