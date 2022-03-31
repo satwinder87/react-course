bb27bafb9acebb5aa3ed116af9e56067820ea48d
Satwinder <satwinder.singh@telenor.se>
2022-03-31 15:25:15 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ const App = () => {
 
   return (
       <div>
-        <h2>Let's get started! Welcome everyone.</h2>
         <NewExpense onAddExpense={addExpenseHandler}/>
         <Expenses items={expenses}/>
       </div>
